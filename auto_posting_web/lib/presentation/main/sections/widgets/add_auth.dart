4e975e930b0604f8454418f5e2e4aa91ac286198
kmwppp <kmwppp@daum.net>
2026-01-28 00:40:20 +0900

    Feat: 계정 추가 경고 란 추가
    빨간색 글씨 추가

--- auto_posting_web/lib/presentation/main/sections/widgets/add_auth.dart
+++ auto_posting_web/lib/presentation/main/sections/widgets/add_auth.dart
@@ -14,7 +14,19 @@ class AddAuth extends StatelessWidget {
       spacing: 10,
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
-        Text("새 계정 추가", style: context.bodyLarge),
+        Row(
+          children: [
+            Text("새 계정 추가", style: context.bodyLarge),
+            SizedBox(width: 20),
+            Text(
+              "이 계정은 2차 로그인이 없어야 합니다.",
+              style: context.bodyLarge.copyWith(
+                color: Colors.red,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ],
+        ),
         // screenWidth > 600 ? AuthWebRow() : AuthMobileColumn(),
         AuthWebRow(),
       ],
